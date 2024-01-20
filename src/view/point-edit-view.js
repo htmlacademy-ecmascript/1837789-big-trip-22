456a9beeecf38152bb0e65a071677cee26bc6107
Алина Вдовиченко <alinavdovichenko.ru@gmail.com>
2024-01-20 13:41:47 +0500

    Добавит проверку цены в форме создания

--- src/view/point-edit-view.js
+++ src/view/point-edit-view.js
@@ -156,7 +156,7 @@ function createPointEditTemplate({state, allOffers, allDestinations, modeAddForm
           <span class="visually-hidden">Price</span>
           &euro;
         </label>
-        <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="${he.encode(String(basePrice))}">
+        <input class="event__input  event__input--price" id="event-price-1" type="number" name="event-price" value="${he.encode(String(basePrice))} min="1" max="100000" required">
       </div>
 
       <button class="event__save-btn  btn  btn--blue" type="submit" ${isDisabled ? 'disabled' : ''}>${isSaving ? 'Saving...' : 'Save'}</button>
